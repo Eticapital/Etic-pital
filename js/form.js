9e14b199d6b2c6338a98750cbb2c26f5a32dca81
Ana Paula Tello <vexilo@iMac-de-Ana.local>
2017-10-03 17:21:56 -0500

    Botón de cerrar agregado para nuevos campos en el formulario y comentarios en las tags de fondear.html

--- js/form.js
+++ js/form.js
@@ -5,7 +5,12 @@ $('.file-upload .file-button').click(function() {
 });
 
 // Add url button
-var urlInput = '<div class="form-group"><input type="url" class="form-control" value="http://"></div>';
+var newUrl = '<div class="form-row new-field"><div class="col-11"><div class="form-group"><input type="url" class="form-control" value="http://"></div></div><div class="col-1"><button type="button" class="close"><span>&times;</span></button></div></div>';
 $('.add-url-button').click(function() {
-  $(this).before(urlInput);
+  $(this).before(newUrl);
+});
+
+// Close/remove button for new fields
+$(document).on('click', '.new-field .close', function(){
+  $(this).parents().remove('.new-field');
 });

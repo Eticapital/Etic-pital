72074e8b5abacb03e492c2998a7349ead54eb963
Alfonso Bribiesca Espinosa <alfonso@vexilo.com>
2017-12-18 10:07:01 -0600

    correos

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    public $token;
+
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        //
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->greeting('Hola, ' . $notifiable->name)
+            ->subject('Solicitud para recuperar tu contraseña')
+            ->line('Recibiste este correo porque recibimos una petición para resetear tu contraseña.
+                Para actualizarlo da click en el siguiente enlace:')
+            ->action('Resetear contraseña', url(config('app.url').route('password.reset', $this->token, false)))
+            ->line('Si no hiciste ninguna petición para recuperar tu contraseña ignora este correo.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
